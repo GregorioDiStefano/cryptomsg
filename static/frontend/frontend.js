dcead935739651eabd67753acd3e4a5e4180be28
Greg <greg.distefano+github@gmail.com>
2016-01-26 23:36:30 +0100

    Drastic UI improvements

--- static/frontend/frontend.js
+++ static/frontend/frontend.js
@@ -15,20 +15,21 @@ function get_key_and_iv(salt, password) {
         var key = CryptoJS.lib.WordArray.create(output.words.slice(0, keySize/32));
         var iv = CryptoJS.lib.WordArray.create(output.words.slice(keySize/32));
 
-        return { "key": key, "iv": iv }
+        return { "key": key, "iv": iv };
 }
 
+
 privnote.controller('encryption', ['$scope', "$rootScope", "$http", "ngDialog", function($scope, $rootScope, $http, ngDialog){
-    $scope.file_set = false
-    $scope.text_set = false
-    $scope.id = ""
+    $scope.file_set = false;
+    $scope.text_set = false;
+    $scope.id = "";
 
-    var port = (location.port == 80 || location.port == 443) ? "" : ":" + location.port
-    $scope.url = location.protocol + "//" + location.hostname + port + "/"
+    var port = (location.port == 80 || location.port == 443) ? "" : ":" + location.port;
+    $scope.url = location.protocol + "//" + location.hostname + port + "/";
 
-    var filename_to_encrypt = ""
-    var file_to_encrypt_data = ""
-    var one_time_read
+    var filename_to_encrypt = "";
+    var file_to_encrypt_data = "";
+    var one_time_read;
 
     $rootScope.$on('ngDialog.opened', function (e, $dialog) {
         var qrcode = new QRCode("qrcode", {
@@ -42,21 +43,28 @@ privnote.controller('encryption', ['$scope', "$rootScope", "$http", "ngDialog", 
     });
 
     $scope.submit = function() {
-        var plaintext = $scope.data ? $scope.data : file_to_encrypt_data
-        var password = $scope.password
+        if (!$scope.password) {
+            return;
+        } else if (!$scope.file_set && !$scope.text_set) {
+            sweetAlert("Error", "No message entered or file selected!");
+            return;
+        }
+
+        var plaintext = $scope.data ? $scope.data : file_to_encrypt_data;
+        var password = $scope.password;
 
         var salt = CryptoJS.lib.WordArray.random(128/8);
-        var data = get_key_and_iv(salt, password)
+        var data = get_key_and_iv(salt, password);
 
-        var key = data["key"]
-        var iv = data["iv"]
+        var key = data.key;
+        var iv = data.iv;
 
         var rawEnc = CryptoJS.AES.encrypt( plaintext,
                                            CryptoJS.enc.Hex.parse(key),
                                            { "iv": iv, "mode": CryptoJS.mode.CBC });
 
         var base64_payload = CryptoJS.enc.Base64.stringify(rawEnc.ciphertext);
-        var base64_salt = CryptoJS.enc.Base64.stringify(salt)
+        var base64_salt = CryptoJS.enc.Base64.stringify(salt);
 
 
         $http({
@@ -69,7 +77,7 @@ privnote.controller('encryption', ['$scope', "$rootScope", "$http", "ngDialog", 
             headers: {'Content-Type': 'application/json'}
           }).success(function (data, status, headers, config) {
 
-                $scope.id = data.id
+                $scope.id = data.id;
 
                 ngDialog.open({
                     template: '/static/dialog.html',
@@ -80,81 +88,86 @@ privnote.controller('encryption', ['$scope', "$rootScope", "$http", "ngDialog", 
             }).error(function (data, status, headers, config) {
                 $scope.status = status + ' ' + headers;
             });
-    }
+    };
 
     $scope.set_file = function(file) {
+        var filename, file_obj, filesize;
         try {
-            var filename = event.target.files[0].name
-            var file_obj = event.target.files[0]
-            var filesize = file_obj.size
+            filename = document.getElementById("file_input").files[0].name;
+            file_obj = document.getElementById("file_input").files[0];
+            filesize = file_obj.size;
 
             if (filesize > 5 * 1024 * 1024) {
-                throw "File larger than 5mb"
-                return
+                throw "File larger than 5mb";
+                return;
             }
 
-            filename_to_encrypt = file_obj.name
+            filename_to_encrypt = file_obj.name;
             read = new FileReader();
-            read.readAsBinaryString(file_obj)
+            read.readAsBinaryString(file_obj);
 
             read.onloadend = function() {
-                file_to_encrypt_data = read.result
-            }
+                file_to_encrypt_data = read.result;
+            };
         } catch (e) {
-            filename = undefined
-            file_obj = undefined
-            document.getElementById("file_input").value = ""
+            filename = undefined;
+            file_obj = undefined;
+            document.getElementById("file_input").value = "";
             sweetAlert("Error", e, "error");
         }
 
         if (filename) {
-            $scope.$apply(function() {
-                $scope.file_set = true
-            })
-        } else $scope.$apply(function() {
-              $scope.file_set = false
-          })
-    }
+            $scope.file_set = true;
+            $scope.$digest();
+        } else {
+            $scope.file_set = false;
+            $scope.$digest();
+        }
+    };
 
     $scope.set_text = function(c) {
         if (c)
-            $scope.text_set = true
-        else $scope.text_set = false
-    }
+            $scope.text_set = true;
+        else $scope.text_set = false;
+    };
 
     $scope.delete_on_read = function(c) {
-        one_time_read = c
-    }
+        one_time_read = c;
+    };
 }]),
 
 
 
 privnote.controller('decryption', ['$scope', "$http", function($scope, $http) {
 
+    $scope.spinner = false;
+
     $scope.decrypt = function() {
-        var id = location.pathname
-        var salt = ""
-        var ciphertext = ""
-        var filename = ""
-        $scope.message_view = ""
+        var id = location.pathname;
+        var salt = "";
+        var ciphertext = "";
+        var filename = "";
+        $scope.message_view = "";
 
         var response = function(data) {
+                $scope.spinner = true;
                 $http.get("/get" + id)
                     .then(function(response) {
-                        salt = response.data.salt
-                        ciphertext = response.data.data
-                        filename = response.data.filename
-                        data(salt, ciphertext)
-            })
-        }
+                        salt = response.data.salt;
+                        ciphertext = response.data.data;
+                        filename = response.data.filename;
+                        data(salt, ciphertext);
+            });
+        };
 
         response(function(salt, cipher){
-            ciphertext = CryptoJS.enc.Base64.parse(ciphertext);
-            salt = CryptoJS.enc.Base64.parse(salt)
 
-            var data = get_key_and_iv(salt, $scope.password)
-            var key = data["key"]
-            var iv = data["iv"]
+            ciphertext = CryptoJS.enc.Base64.parse(ciphertext);
+            salt = CryptoJS.enc.Base64.parse(salt);
+
+            var data = get_key_and_iv(salt, $scope.password);
+            var key = data.key;
+            var iv = data.iv;
 
             var plaintext = CryptoJS.AES.decrypt(
               {
@@ -163,16 +176,17 @@ privnote.controller('decryption', ['$scope', "$http", function($scope, $http) {
               },
               CryptoJS.enc.Hex.parse(key),
               { iv: iv, mode: CryptoJS.mode.CBC }
-            )
+            );
 
 
             try {
-                plaintext = plaintext.toString(CryptoJS.enc.Utf8)
-                if (plaintext == "")
-                    throw Error()
+                plaintext = plaintext.toString(CryptoJS.enc.Utf8);
+                if (plaintext === "")
+                    throw Error();
             } catch (e) {
-                alert("Wrong password")
-                return
+                alert("Wrong password");
+                $scope.spinner = false;
+                return;
             }
 
             if (filename) {
@@ -183,8 +197,10 @@ privnote.controller('decryption', ['$scope', "$http", function($scope, $http) {
                 var blob = new Blob([bytes], {type: "application/octet-stream"});
                 saveAs(blob, filename);
             } else {
-                $scope.message_view = plaintext
+                $scope.message_view = plaintext;
             }
-        })
-    }
+
+            $scope.spinner = false;
+        });
+    };
 }]);
